24dffa44397d5a9cf606df3e7aa9c26ec00f878c
Mykyta <nikiforovpizza@gmail.com>
2025-03-21 01:35:34 +0200

    Refactor AuthUtils

--- photomap-core-api/api-gateway/src/main/java/ua/in/photomap/apigateway/util/CookieUtils.java
+++ photomap-core-api/api-gateway/src/main/java/ua/in/photomap/apigateway/util/CookieUtils.java
@@ -1,8 +1,12 @@
 package ua.in.photomap.apigateway.util;
 
-import org.springframework.http.*;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class CookieUtils {
@@ -12,5 +16,11 @@ public class CookieUtils {
                 .maxAge(0)
                 .build();
         response.addCookie(cookie);
+    }
+
+    public static Optional<String> getCookieValue(ServerHttpRequest request, String cookieName) {
+        return Optional.ofNullable(request.getCookies().getFirst(cookieName))
+                .filter(cookie -> !cookie.getValue().isEmpty())
+                .map(HttpCookie::getValue);
     }
 } 
